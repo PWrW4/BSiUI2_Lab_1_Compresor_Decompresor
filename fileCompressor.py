c7b809a4dbfc3b8d0d1c9eb7747e5724840ffe33
Wojciech Wojcik <wojtek.wroclaw@hotmail.com>
2019-10-15 01:24:30 +0200

    working, testing and searching for bugs?

--- fileCompressor.py
+++ fileCompressor.py
@@ -2,11 +2,11 @@ import numpy as np
 import sys
 import math
 
-file_in_name = "test"  # test is from laboratory example
-file_out_name = "test.wca"  # Wojciech compression algorithm xD
+file_in_name = 'test'  # test is from laboratory example
+file_out_name = 'test.wca'  # Wojciech compression algorithm xD
 
-f = open(file_in_name, 'rb')
-o = open(file_out_name, 'wb')
+in_file = open(file_in_name, 'rb')
+out_file = open(file_out_name, 'wb')
 
 # array for setting chars
 dict_array = [0] * 256
@@ -18,15 +18,15 @@ x = 0
 k = 0
 
 while True:
-    piece = f.read(1)
+    piece = in_file.read(1)
     if not piece:
         break
     dict_array[int(ord(piece))] = 1
     k = k + 1
 
-f.close()
+in_file.close()
 
-f = open(file_in_name, 'r')
+in_file = open(file_in_name, 'r')
 
 # dictionary length
 x = dict_array.count(1)
@@ -35,44 +35,47 @@ x = dict_array.count(1)
 N = math.ceil(math.log(x, 2))
 
 if x >= 128:
-    f.close()
-    o.close()
-    print("to many chars, compression doesnt make any sens")
+    in_file.close()
+    out_file.close()
+    print('to many chars, compression doesnt make any sens')
     sys.exit()
 
 chars_in_int_array = np.where(np.array(dict_array) == 1)[0]
 
-o.write(bytes([x]))
+out_file.write(bytes([x]))
 
-o.write(bytes(list(chars_in_int_array)))
+out_file.write(bytes(list(chars_in_int_array)))
 
 # rest of bits at the end of file
 R = (8 - (3 + (k * N)) % 8) % 8
 
-print("Liczba znaków: " + str(x))
-print("Liczba potrzebnych bitów na jeden znak: " + str(N))
-print("Długość tekstu: " + str(k))
-print("Nadmiarowe bity: " + str(R))
+print('Liczba znaków: ' + str(x))
+print('Liczba potrzebnych bitów na jeden znak: ' + str(N))
+print('Długość tekstu: ' + str(k))
+print('Nadmiarowe bity: ' + str(R))
 
 str_to_write = format(R, '03b')
 
 while True:
-    piece = f.read(1)
+    piece = in_file.read(1)
     if not piece:
         break
     if len(str_to_write) >= 8:
-        o.write(bytes([int(str_to_write[:8], 2)]))
+        out_file.write(bytes([int(str_to_write[:8], 2)]))
         str_to_write = str_to_write[8:]
     piece_ascii = ord(piece)
     piece_order = list(chars_in_int_array).index(piece_ascii)
     str_to_write += format(piece_order, '02b')
 
 if len(str_to_write) != 0:
-    while len(str_to_write) != 8:
+    while len(str_to_write) % 8 != 0:
         str_to_write += '1'
 
-    o.write(bytes([int(str_to_write[:8], 2)]))
-    str_to_write = str_to_write[8:]
+    while len(str_to_write) != 0:
+        out_file.write(bytes([int(str_to_write[:8], 2)]))
+        str_to_write = str_to_write[8:]
 
-f.close()
-o.close()
+in_file.close()
+out_file.close()
+
+print('Compressed in file: ' + file_out_name)
--- fileDeCompressor.py
+++ fileDeCompressor.py
@@ -1,26 +1,51 @@
 import math
 
-f = open("4char_100MB.dat", 'r')
+file_without_extension = 'test'  # file base name
 
-# dictionary length
-x = ord(f.read(1))
+file_in_name = file_without_extension + '.wca'  # Wojciech compression algorithm xD
+file_out_name = file_without_extension + '_DeCompressed'  # out file
+
+in_file = open(file_in_name, 'rb')
+out_file = open(file_out_name, 'wb')
+
+# dict length
+x = int.from_bytes(in_file.read(1), byteorder='big')
 
 # number of bits that represent one dictionary char
 N = math.ceil(math.log(x, 2))
 
-# array for setting chars
-dict_array = [0] * 256
+file_dict = {}
 
-# text length
-k = 0
+for i in range(x):
+    file_dict.update({format(i, '0' + str(N) + 'b'): chr(int.from_bytes(in_file.read(1), byteorder='big'))})
+
+print('Dict from file: ' + str(file_dict))
+print('Dict length: ' + str(x))
+print('Dict one char length in bits in input file: ' + str(N))
+
+bytes_from_file_buffer = format(int.from_bytes(in_file.read(1), byteorder='big'), '08b')
+
+# rest bytes at the end of file (not information, just filling to 8 bits in compression)
+R = int(bytes_from_file_buffer[:3], 2)
+
+bytes_from_file_buffer = bytes_from_file_buffer[3:]
 
 while True:
-    piece = f.read(1)
+    piece = in_file.read(1)
     if not piece:
         break
-    k = k + 1
-    dict_array[int(ord(piece))] = 1
+    bytes_from_file_buffer += format(int.from_bytes(piece, byteorder='big'), '08b')
+    while len(bytes_from_file_buffer) > 8:
+        out_file.write(file_dict.get(bytes_from_file_buffer[:N]).encode('ascii'))
+        bytes_from_file_buffer = bytes_from_file_buffer[N:]
 
-print(k)
-print(dict_array)
-f.close()
+bytes_from_file_buffer = bytes_from_file_buffer[:len(bytes_from_file_buffer)-R]
+
+while len(bytes_from_file_buffer) != 0:
+    out_file.write(file_dict.get(bytes_from_file_buffer[:N]).encode('ascii'))
+    bytes_from_file_buffer = bytes_from_file_buffer[N:]
+
+in_file.close()
+out_file.close()
+
+print('Decompressed in file' + file_out_name)
